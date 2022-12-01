dd0774486d20486d7873083593e3b092796e637c
joeflack4 <joeflack4@gmail.com>
2022-12-01 14:26:44 -0500

    .gitignore: Added .DS_Store. Organized into sections: (i) directories, (ii) dot files, (iii) related dirs/files/patterns

--- src/oaklib/converters/obo_graph_to_fhir_converter.py
+++ src/oaklib/converters/obo_graph_to_fhir_converter.py
@@ -1,3 +1,8 @@
+"""OboGraph to FHIR Converter
+
+Resources
+- Updates issue: https://github.com/INCATools/ontology-access-kit/issues/369
+"""
 import logging
 from dataclasses import dataclass
 from typing import Any, Dict, List, Tuple
@@ -48,7 +53,10 @@ SCOPE_DISPLAY = {
 class OboGraphToFHIRConverter(DataModelConverter):
     """Converts from OboGraph to FHIR."""
 
-    def dump(self, source: GraphDocument, target: str = None) -> None:
+    # TODO: `retention` is temporary. I think we likely have to give this a different name/functionality, as well as put
+    #  it in the CLI and possibly elsewhere. I'm also doing this a pure way, passing it to dump and then further down,
+    #  rather than an instance property. - Joe 2022/12/01
+    def dump(self, source: GraphDocument, target: str = None, retention: str = ['maximal', 'minimal'][0]) -> None:
         """
         Dump an OBO Graph Document to a FHIR CodeSystem
 
@@ -56,7 +64,7 @@ class OboGraphToFHIRConverter(DataModelConverter):
         :param target:
         :return:
         """
-        cs = self.convert(source)
+        cs = self.convert(source, retention=retention)
         json_str = json_dumper.dumps(cs)
         if target is None:
             print(json_str)
@@ -64,7 +72,9 @@ class OboGraphToFHIRConverter(DataModelConverter):
             with open(target, "w", encoding="UTF-8") as f:
                 f.write(json_str)
 
-    def convert(self, source: GraphDocument, target: CodeSystem = None) -> CodeSystem:
+    def convert(
+        self, source: GraphDocument, target: CodeSystem = None, retention: str = ['maximal', 'minimal'][0]
+    ) -> CodeSystem:
         """
         Convert an OBO Graph Document to a FHIR CodingSystem
 
@@ -76,10 +86,11 @@ class OboGraphToFHIRConverter(DataModelConverter):
             target = CodeSystem()
         target.resourceType = CodeSystem.__name__
         for g in source.graphs:
-            self._convert_graph(g, target=target)
+            self._convert_graph(g, target=target, retention=retention)
         return target
 
     def code(self, uri: CURIE) -> str:
+        """Convert a code"""
         if not self.curie_converter:
             return uri
         curie = self.curie_converter.compress(uri)
@@ -88,29 +99,32 @@ class OboGraphToFHIRConverter(DataModelConverter):
         else:
             return curie
 
-    def _convert_graph(self, source: Graph, target: CodeSystem) -> CodeSystem:
+    def _convert_graph(
+        self, source: Graph, target: CodeSystem, retention: str = ['maximal', 'minimal'][0]
+    ) -> CodeSystem:
         target.id = source.id
         edges_by_subject = index_graph_edges_by_subject(source)
         logging.info(f"Converting graph to obo: {source.id}, nodes={len(source.nodes)}")
         for n in source.nodes:
             logging.debug(f"Converting node {n.id}")
-            self._convert_node(n, index=edges_by_subject, target=target)
+            self._convert_node(n, index=edges_by_subject, target=target, retention=retention)
         return target
 
     def _convert_node(
-        self, source: Node, index: Dict[CURIE, List[Edge]], target: CodeSystem
+        self, source: Node, index: Dict[CURIE, List[Edge]], target: CodeSystem,
+        retention: str = ['maximal', 'minimal'][0]
     ) -> Concept:
-        id = self.code(source.id)
-        logging.debug(f"Converting node {id} from {source}")
-        concept = Concept(code=id, display=source.lbl)
+        _id = self.code(source.id)
+        logging.debug(f"Converting node {_id} from {source}")
+        concept = Concept(code=_id, display=source.lbl)
         target.concept.append(concept)
         if source.meta:
             self._convert_meta(source, concept)
         for e in index.get(source.id, []):
             obj = self.code(e.obj)
-            if e.pred in DIRECT_PREDICATE_MAP:
+            if retention == 'maximal' or e.pred in DIRECT_PREDICATE_MAP:
                 concept.property.append(
-                    ConceptProperty(code=DIRECT_PREDICATE_MAP[e.pred], valueCode=obj)
+                    ConceptProperty(code=DIRECT_PREDICATE_MAP.get(e.pred, e.pred), valueCode=obj)
                 )
             else:
                 logging.debug(f"Skipping edge {e}")
